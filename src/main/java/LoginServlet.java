f1531f2a4bfb829c007023f9f2bb1791f6614c17
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-05-24 16:01:00 -0500

    Sessions and Cookies exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -5,10 +5,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+@WebServlet(name = "LoginServlet", urlPatterns = {"/login", "/logout"})
 public class LoginServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        String requestURI = request.getRequestURI();
+
+        if ("/login".equals(requestURI)) {
+            // Check if user is already logged in, redirect to profile page
+            if (isLoggedIn(request)) {
+                response.sendRedirect("/profile");
+                return;
+            }
+
+            // User is not logged in, show login page
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        } else if ("/logout".equals(requestURI)) {
+            // Logout the user and redirect to login page
+            request.getSession().invalidate();
+            response.sendRedirect("/login");
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -17,9 +33,16 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            // Set "user" key in session to mark user as logged in
+            request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
     }
+
+    private boolean isLoggedIn(HttpServletRequest request) {
+        return request.getSession().getAttribute("user") != null;
+    }
 }
+
--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -7,7 +7,22 @@ import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (!isLoggedIn(request)) {
+            response.sendRedirect("/login");
+            return;
+        }
+
+        // User is logged in, show profile page
+        String username = (String) request.getSession().getAttribute("user");
+
+        request.setAttribute("username", username);
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
+
+    private boolean isLoggedIn(HttpServletRequest request) {
+        return request.getSession().getAttribute("user") != null;
+    }
 }
+
