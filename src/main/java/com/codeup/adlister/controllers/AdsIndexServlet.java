1bc352e12bf5b8943cf58f3f4fd7f9354c1b3ace
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-05-25 11:33:10 -0500

    Passwords Exercise

--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -1,18 +1,46 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.Ads;
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ads")
 public class AdsIndexServlet extends HttpServlet {
+    private final Ads adsDao;
+
+    public AdsIndexServlet() {
+        adsDao = DaoFactory.getAdsDao();
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.setAttribute("ads", adsDao.all());
         request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+
+        Ad ad = new Ad(user.getId(), title, description);
+        adsDao.insert(ad);
+
+        response.sendRedirect("/ads/" + ad.getId());
     }
 }
