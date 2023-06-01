f719e360de9b36fa856b07a093abbf0cf0a81a2a
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-06-01 09:08:44 -0500

    Logout Servlet

---
+++ src/main/java/dao/Config.java
@@ -0,0 +1,29 @@
+package dao;
+
+public class Config {
+    private String url;
+    private String username;
+    private String password;
+
+    public Config(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Config() {
+
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
